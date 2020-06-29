9786bad6227d00340299bbaa9f24430eac2b7e73
Ravi Basil <ravi09basil@rediffmail.com>
2020-06-29 23:09:25 +0530

    Updates

---
+++ precache-manifest.e070e89b0dfad6d500aa2596a48edec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b4397fd8ca1b3911ab157c30adcaaa",
+    "url": "//ravibasil.github.io/.github.io/crownstack-test/index.html"
+  },
+  {
+    "revision": "5bc3670397b4413baf9c",
+    "url": "//ravibasil.github.io/.github.io/crownstack-test/static/css/main.5367f753.chunk.css"
+  },
+  {
+    "revision": "fbf642fda90bd0179b1e",
+    "url": "//ravibasil.github.io/.github.io/crownstack-test/static/js/2.11fd327c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "//ravibasil.github.io/.github.io/crownstack-test/static/js/2.11fd327c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bc3670397b4413baf9c",
+    "url": "//ravibasil.github.io/.github.io/crownstack-test/static/js/main.f6ec49eb.chunk.js"
+  },
+  {
+    "revision": "b2cb4a0cbbcea78f9df1",
+    "url": "//ravibasil.github.io/.github.io/crownstack-test/static/js/runtime-main.201da8ef.js"
+  }
+]);
